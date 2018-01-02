8504626def2e80dc8cd8e51ce5849b3bddcfa94c
hetoney <hetoney@foxmail.com>
2018-01-02 16:20:33 +0800

    更新目录

---
+++ 04 SQLServer2005基础/第6章 课程设计/上机作业/17033041160杜鹏/创建超市管理库.sql
@@ -0,0 +1,15 @@
+
+create database Supermarket on(
+name = 'marketmdf',
+filename = 'C:\Supermarket.mdf',
+size = 3MB,
+maxsize = 50MB,
+filegrowth = 10MB
+)
+log on (
+name = 'marketldf',
+filename = 'C:\Supermarket_log.ldf',
+size = 2MB,
+maxsize = 50MB,
+filegrowth = 10%
+)
